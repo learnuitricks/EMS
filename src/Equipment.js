00d49d7effb000105cac446bca3d9c83bd05d690
uitricks <learn.uitricks@gmail.com>
2021-03-28 09:36:23 +0530

    added equipmentcomponent

--- src/Equipment.js
+++ src/Equipment.js
@@ -1,8 +1,22 @@
-function Equipment(){
-    return (
-        <>
-        <div> i am equipment component</div>
-        </>
-    )
+import {
+    Link
+  } from "react-router-dom";
 
+function Equipment(props){
+    return(  <tr key={props.itemIndex}>
+                      
+        <td>{props.itemIndex+1}</td>
+
+
+  
+         <td> {props.equipment.id}</td>
+         <td> 
+   <Link to={`/details/${props.equipment.name}`}>{props.equipment.name}</Link>
+ </td>
+        
+        <td>{props.equipment.date}</td>
+       <td> {props.equipment.maintenanceCycle}</td>
+</tr>)
 }
+
+export default Equipment;
